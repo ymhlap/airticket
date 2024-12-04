96a1d950a676b5db39857b82850c161d092aa443
BCXHBDPDX <2183640376@qq.com>
2024-12-04 19:13:28 +0800

    机票实体类

---
+++ airticket_common/src/main/java/com/ymh/airticket/entity/Flightschedule.java
@@ -0,0 +1,53 @@
+package com.ymh.airticket.entity;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName flightschedule
+ */
+@TableName(value ="flightschedule")
+@Data
+public class Flightschedule implements Serializable {
+    /**
+     * 
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer scheduleid;
+
+    /**
+     * 
+     */
+    private Integer flightid;
+
+    /**
+     * 
+     */
+    private String departureairport;
+
+    /**
+     * 
+     */
+    private String arrivalairport;
+
+    /**
+     * 
+     */
+    private Date departuredatetime;
+
+    /**
+     * 
+     */
+    private Date arrivaldatetime;
+
+    /**
+     * 
+     */
+    private Date flightduration;
+
+
+}
